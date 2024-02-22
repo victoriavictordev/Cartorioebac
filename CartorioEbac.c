6fb666988130594ad79a6dd6f78a821507256bf1
victoriavictordev <160890884+victoriavictordev@users.noreply.github.com>
2024-02-22 16:36:55 +0000

    Opção de saida
    Incluso no sistema a opção de saída do sistema

--- CartorioEbac.c
+++ CartorioEbac.c
@@ -133,7 +133,7 @@ int main()
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes\n");
 	    printf("\t3 - Deletar nomes\n\n");
-	    printf("Opção: ");
+	    printf("\t4 - Sair do sistema\n\n");
 	
 	    scanf("%d",&opcao);
 	
@@ -151,6 +151,11 @@ int main()
 	    	
 	    	case 3:
 	    	deletar();
+			break;
+			
+			case 4:
+			printf("Obrigado por utilizar o sistema!\n");
+			return 0;
 			break;
 			
 			default:
